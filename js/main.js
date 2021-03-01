dce792dc651eb2bd616276ebe8ca9e8827c7a1d9
Febyasashige <febyasashige@gmail.com>
2021-03-01 17:24:33 +0900

    overview画像の追加、CSSの調整を行いました

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.addEventListener('DOMContentLoaded', function() {
     var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
         paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
